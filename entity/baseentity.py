76b816280ad3ed924f68e2f5e61c5264520882cf
Emerson Jacobson <emwjacobson@gmail.com>
2020-06-12 23:27:52 -0700

    Added documentation for BaseEntity and BaseWorld

--- entity/baseentity.py
+++ entity/baseentity.py
@@ -1,5 +1,4 @@
 import pygame
-# from worlds.baseworld import BaseWorld
 
 
 class BaseEntity:
@@ -10,36 +9,88 @@ class BaseEntity:
     _counter: float
 
     def __init__(self, pos=[0, 0], angle=0, speed=0):
+        """Initializes a new BaseEntity.
+
+        Args:
+            pos (list, optional): Starting position of the entity relative to the world. Defaults to [0, 0].
+            angle (int, optional): Angle that the entity is facing. 0 is to the right, and rotates clockwise. Defaults to 0.
+            speed (int, optional): Speed that the entity is moving. Defaults to 0.
+        """
         self._pos = pos
         self._angle = angle
         self._speed = speed
         self._counter = 0
 
     def get_x(self):
+        """Gets the x position of the entity, relative to the world
+
+        Returns:
+            int: The x position of the entity
+        """
         return self._pos[0]
 
     def get_y(self):
+        """Gets the y position of the entity, relative to the world
+
+        Returns:
+            int: The y position of the entity
+        """
         return self._pos[1]
 
     def set_x(self, x):
+        """Sets the x position of the entity, relative to the world
+
+        Args:
+            x (int): The x position of the entity
+        """
         self._pos[0] = x
 
     def set_y(self, y):
+        """Sets the y position of the entity, relative to the world
+
+        Args:
+            y (int): The y position of the entity
+        """
         self._pos[1] = y
 
     def get_width(self):
+        """Gets the width of the entity in pixels
+
+        Returns:
+            int: Width of the entity in pixels
+        """
         return self._entity_surface.get_width()
 
     def get_height(self):
+        """Gets the height of the entity in pixels
+
+        Returns:
+            int: Height of the entity in pixels
+        """
         return self._entity_surface.get_height()
 
     def get_pos(self):
-        return (self._x, self._y)
+        """Gets the x, y position of the entity, relative to the world
+
+        Returns:
+            list[int]: The x, y position of the entity
+        """
+        return [self._x, self._y]
 
     def set_speed(self, speed):
+        """Gets the speed of the entity
+
+        Args:
+            speed (int): The speed to set the entity to
+        """
         self._speed = speed
 
     def set_angle(self, angle):
+        """Sets the angle that the entity is moving at
+
+        Args:
+            angle (int): The angle in degrees
+        """
         # TODO: Make this use modulo
         if angle < 0:
             self.set_angle(360 + angle)
@@ -49,15 +100,37 @@ class BaseEntity:
             self._angle = angle
 
     def get_angle(self):
+        """Get the angle in degrees of the entity
+
+        Returns:
+            int: The angle the entity is facing in degrees
+        """
         return self._angle
 
     def handle_events(self, events, world):
+        """Handles events for an entity.
+
+        Args:
+            events (list[pygame.event.Event]): A list of pygame Events
+            world (BaseWorld): The world that the entity belongs to
+        """
         pass
 
     def update(self, micro, world):
+        """Updates the entity. Micro is passed to make different FPSs behave the same.
+
+        Args:
+            micro (float): Time in seconds that the last frame too.
+            world (BaseWorld): The world that the entity belongs to
+        """
         self._counter = (self._counter + micro) % 1
 
     def render(self, surface):
+        """Renders the entity to a given surface
+
+        Args:
+            surface (pygame.Surface): The surface to render the entity to
+        """
         pass
 
 # ad	speed = 0		angle = _
--- worlds/baseworld.py
+++ worlds/baseworld.py
@@ -17,16 +17,8 @@ class BaseWorld:
         Args:
             width (int, optional): The width of the world. Defaults to 500.
             height (int, optional): The height of the world. Defaults to 500.
-            x (int, optional): The starting x position, relative to the window. Defaults to 0.
+            x (int, optional): The starting position, relative to the upper left of the window. Defaults to [0, 0].
         """
-        # Initializes a new BaseWorld. Worlds are made up of 2 layers, a
-        # background surface and a
-
-        # @param width The width of the world
-        # @param height The height of the world
-        # @param x The initial x position of the world
-        # @param y The initial y position of the world
-        #
         self._width = width
         self._height = height
         self._pos = pos
@@ -37,31 +29,63 @@ class BaseWorld:
         self.populate_world()
 
     def init_background(self):
-        """
+        """Overload this function in a subclass to populate the background_surface.
         """
         pass
 
     def populate_world(self):
+        """Overload this function in a subclass to populate the world with entities
+        """
         pass
 
     def get_width(self):
+        """Gets the width of the current world
+
+        Returns:
+            int: Width, in pixels, of the world
+        """
         return self._width
 
     def get_height(self):
+        """Gets the height of the current world
+
+        Returns:
+            int: Height, in pixels, of the world
+        """
         return self._height
 
     def get_entities(self):
+        """Gets the entities in the current world
+
+        Returns:
+            list[BaseEntity]: A list of BaseEntity objects in the world
+        """
         return self._entities
 
     def handle_events(self, events):
+        """Handles the events for the world, also calls handle_events for every entity in the world
+
+        Args:
+            events (list[pygame.event.Event]): A list of pygame events
+        """
         for e in self._entities:
             e.handle_events(events, self)
 
     def update(self, micro):
+        """Update all entities
+
+        Args:
+            micro (float): Number of seconds that it took to render the frame. Used to make any FPS run at the same speed.
+        """
         for e in self._entities:
             e.update(micro, self)
 
     def render(self, screen):
+        """Renders the world. Starts with the background_surface then the entity_surface over top.
+
+        Args:
+            screen (pygame.Surface): The surface for the world to be rendered to
+        """
         f_world = pygame.Surface((self._width, self._height))
 
         # Render map background
